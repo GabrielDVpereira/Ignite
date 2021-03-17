ac1a1c05598d32a7e233ca64b469bc338d9f0e0f
Gabriel Davi <gdavi.gd@gmail.com>
2021-03-17 14:02:33 -0300

    configrue mirageJS

--- projects/dtmoney/src/App.tsx
+++ projects/dtmoney/src/App.tsx
@@ -1,6 +1,25 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amout: 400,
+          type: "deposit",
+          category: "food",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 export function App() {
   return (
