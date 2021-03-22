1a91beee9f2e151d22fc8035f8d03ee4e07ca33e
Gabriel Davi <gdavi.gd@gmail.com>
2021-03-22 11:15:25 -0300

    set up new app ignews

---
+++ projects/ignews/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+// loaded on every page change
+import { AppProps } from "next/app";
+import "../styles/global.scss";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
