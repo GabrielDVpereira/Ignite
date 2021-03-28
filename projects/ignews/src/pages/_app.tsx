2fba1a958afd0531fc9247e630f368aa34e9f27a
Gabriel Davi <gdavi.gd@gmail.com>
2021-03-28 18:17:59 -0300

    set github auth

--- projects/ignews/src/pages/_app.tsx
+++ projects/ignews/src/pages/_app.tsx
@@ -2,13 +2,14 @@
 import { AppProps } from "next/app";
 import "../styles/global.scss";
 import { Header } from "../components/Header";
+import { Provider as AuthProvider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header></Header>
+    <AuthProvider session={pageProps.session}>
+      <Header />
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
 
