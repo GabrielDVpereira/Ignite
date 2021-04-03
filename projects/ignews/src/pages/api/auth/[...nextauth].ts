1cecdcb8f3bb727a93bd922abd72feb154432941
Gabriel Davi <gdavi.gd@gmail.com>
2021-04-03 19:07:21 -0300

    create post page

--- projects/ignews/src/pages/api/auth/[...nextauth].ts
+++ projects/ignews/src/pages/api/auth/[...nextauth].ts
@@ -12,6 +12,26 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    //callbacks are called when a user performs a auth action
+    async session(session) {
+      const userActiveSubscription = await fauna.query(
+        q.Get(
+          q.Match(
+            q.Index("subscription_by_user_ref"),
+            q.Select(
+              "ref",
+              q.Get(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(session.user.email)
+                )
+              )
+            )
+          )
+        )
+      );
+      return session;
+    },
     async signIn(user, account, profile) {
       const { email } = user;
       try {
