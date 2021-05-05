014b218e1c1f5085c5e06367d0837ef1c673dd09
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-04 23:18:36 -0300

    finish header

--- projects/dashgo/src/components/Header.tsx
+++ projects/dashgo/src/components/Header.tsx
@@ -1,8 +1,14 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'; 
-import { RiSearch2Line } from 'react-icons/ri'
-export function Header(){
-  return(
-    <Flex 
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+
+import {
+  RiNotification2Line,
+  RiSearch2Line,
+  RiUserAddLine,
+} from "react-icons/ri";
+
+export function Header() {
+  return (
+    <Flex
       as="header"
       w="100%"
       maxWidth={1480}
@@ -12,14 +18,11 @@ export function Header(){
       px="6"
       align="center"
     >
-      <Text
-        fontSize="3xl"
-        fontWeight="bold"
-        letterSpacing="tight"
-        w="64"
-      >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
         dashgo
-      <Text as="span" color="pink.500" ml="1">.</Text>
+        <Text as="span" color="pink.500" ml="1">
+          .
+        </Text>
       </Text>
       <Flex
         as="label"
@@ -34,17 +37,41 @@ export function Header(){
         bg="gray.800"
         borderRadius="full"
       >
-        <Input 
-          color="gay.50" 
+        <Input
+          color="gay.50"
           variant="unstyled"
           px="4"
           mr="4"
           placeholder="Buscar na plataforma"
-          _placeholder={{color: 'gray.400'}}
+          _placeholder={{ color: "gray.400" }}
         />
-        <Icon as={RiSearch2Line} fontSize="20"/>
+        <Icon as={RiSearch2Line} fontSize="20" />
       </Flex>
-    </Flex> 
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotification2Line} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
 
-  ); 
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Diego Fernandes</Text>
+            <Text color="gray.300" fontSize="small">
+              Diego.shell.f@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Gabriel Davi" />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
