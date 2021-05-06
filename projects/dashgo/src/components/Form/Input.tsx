54c1de50c0958e48efec40b41d18ee41e0e70b48
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-06 20:09:13 -0300

    start create user page

--- projects/dashgo/src/components/Form/Input.tsx
+++ projects/dashgo/src/components/Form/Input.tsx
@@ -1,27 +1,30 @@
-import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps} from '@chakra-ui/react'
+import {
+  FormLabel,
+  FormControl,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
 
-
-interface InputProps extends ChakraInputProps{
+interface InputProps extends ChakraInputProps {
   name: string;
-  label?: string; 
-  type: string; 
+  label?: string;
 }
-export function Input({name, label, ...rest}: InputProps){
-  return(
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
     <FormControl>
       {label && <FormLabel html={name}>{label}</FormLabel>}
-      <ChakraInput 
+      <ChakraInput
         id={name}
-        name={name} 
+        name={name}
         focusBorderColor="pink.500"
-          bgColor="gray.900"
-          variant="filled"
-          _hover={{
-            bgColor: 'gray.900'
-          }}
-          size="lg"
-          {...rest}
-        />
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      />
     </FormControl>
-  )
+  );
 }
