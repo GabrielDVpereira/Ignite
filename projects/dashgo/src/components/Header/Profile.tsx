dbe3ba52869d756341ffd28c0ef605e5b155bfb9
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-11 23:11:49 -0300

    add responsivity to header

--- projects/dashgo/src/components/Header/Profile.tsx
+++ projects/dashgo/src/components/Header/Profile.tsx
@@ -1,13 +1,23 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface profileProps {
+  showProfileData?: boolean; 
+}
+export function Profile({showProfileData = true}: profileProps) {
   return (
     <Flex align="center">
       <Box mr="4" textAlign="right">
-        <Text>Diego Fernandes</Text>
-        <Text color="gray.300" fontSize="small">
-          Diego.shell.f@gmail.com
-        </Text>
+        {
+          showProfileData && (
+          <>
+            <Text>Diego Fernandes</Text>
+            <Text color="gray.300" fontSize="small">
+              Diego.shell.f@gmail.com
+            </Text>
+          </>
+          )
+        }
+        
       </Box>
 
       <Avatar size="md" name="Gabriel Davi" />
