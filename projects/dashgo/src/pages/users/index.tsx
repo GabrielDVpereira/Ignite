87b37bc83cc7051eb2f45b15ed6567d435ac7594
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-10 21:28:41 -0300

    creating a hook for the users

--- projects/dashgo/src/pages/users/index.tsx
+++ projects/dashgo/src/pages/users/index.tsx
@@ -19,34 +19,14 @@ import { RiAddLine, RiPencilFill } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
-import { useQuery } from 'react-query'
 import Link from 'next/link';
 import { Loading } from "../../components/Loading";
+import { useUsers } from "../../services/hooks/useUSers";
 
 
 export default function UserList() {
-  // useQuery generate a cached query, using a strategy called stale while revalidate, where the browser will still make an http request but show the latest data while fetching
-  // useQuery has also an feature called revalidate on focus, wich means that whenever the user access the browser, the http request will be performed
-  const { data, isLoading, error, isFetching } = useQuery('users', async () => { // we set a name for the query becase that's the name of the caching later, then a function that will return the data that will be stored
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
 
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name, 
-        email: user.email, 
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit', 
-          month: 'long', 
-          year: 'numeric'
-        })
-      }
-    })
-    return users // data that will be saved into our code
-  },{
-    staleTime: 1000 * 5  // define a time for our data to be considered "stale", then the reactQuery will fetch the new info again. If not passed, every data will be considered stale as soon as it's fetched and then the reactQuery will make a request on every focus 
-  })
+  const { data, isLoading, error, isFetching } = useUsers()
 
   const isWideVersion = useBreakpointValue({
     base: false,
