9856945bec626283899152dcf759c5a8f07e5829
Gabriel Davi <gdavi.gd@gmail.com>
2021-06-19 23:24:30 -0300

    set token on request

--- projects/auth-next/services/api.ts
+++ projects/auth-next/services/api.ts
@@ -1,28 +1,14 @@
 import axios from 'axios'; 
 import { parseCookies,setCookie } from 'nookies'; 
-import jwt, { JwtPayload } from 'jsonwebtoken'; 
-
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333', 
 });
 
-const whiteList = ['/refresh']
 
 api.interceptors.request.use(async (config) => {
   const cookies = parseCookies(); 
   let token = cookies['nextauth.token']; 
-  console.log(config.url, token)
-
-  if(token && !whiteList.includes(String(config.url))){
-    const payload = jwt.decode(token) as JwtPayload; 
-    const tokenExpirationTime = payload.exp as number; 
-    const now = Date.now() / 1000;
-    if(now > tokenExpirationTime){
-      const refreshToken = cookies['nextauth.refreshToken']; 
-      token = await getNewToken(refreshToken); 
-    }
-  }
   config.headers.authorization =  `Bearer ${token}`
   return config
 }, (err) => {
@@ -30,11 +16,10 @@ api.interceptors.request.use(async (config) => {
 }); 
 
 
-const getNewToken = async (refreshToken: string): Promise<string> => {
+const getNewToken = async (refreshToken: string): Promise<AuthTokens> => {
   const { data } = await api.post('/refresh', { refreshToken })
     const { token: newToken, refreshToken: newRefreshToken } = data; 
-    saveTokensInCookies({ token:newToken , refreshToken: newRefreshToken  }); 
-    return newToken;
+    return { token: newToken, refreshToken: newRefreshToken };
 }
 
 interface AuthTokens {
@@ -43,12 +28,14 @@ interface AuthTokens {
 }
 
 const saveTokensInCookies = ({ token, refreshToken }:AuthTokens ) => {
-  setCookie(undefined, 'nextauth.token', token, {
-    maxAge: 60 * 60 * 24 * 30, // 1 month
-    path: '/' // paths that can access the cookie
-  }); 
-  setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
-    maxAge: 60 * 60 * 24 * 30, // 1 month
-    path: '/' // paths that can access the cookie
-  }); 
+  return new Promise((resolve) => {
+    setCookie(undefined, 'nextauth.token', token, {
+      maxAge: 60 * 60 * 24 * 30, // 1 month
+      path: '/' // paths that can access the cookie
+    }); 
+    setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+      maxAge: 60 * 60 * 24 * 30, // 1 month
+      path: '/' // paths that can access the cookie
+    }); 
+  })  
 }
